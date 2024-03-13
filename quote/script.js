660311bc36f36d9379b7edff9aab1de92c3ae67d
Nenad T <trajkovik.nenad@gmail.com>
2024-03-13 19:28:42 +0100

    fix: heroku endpoint

--- quote/script.js
+++ quote/script.js
@@ -1,6 +1,6 @@
 async function fetchQuote() {
     try {
-        const response = await fetch('https://cors-anywhere.herokuapp.com/' +'https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en');
+        const response = await fetch('https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en');
         const data = await response.json();
         document.getElementById('quote').innerHTML = `${data.quoteText} by ${data.quoteAuthor}`;
     } catch(error) {
